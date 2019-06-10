55064d3bc3b2d7e87fb2bfb4b233056664da9955
LandiMi2 <landycofia@gmail.com>
2019-06-10 18:16:25 +0300

    script & modules(functions)

---
+++ Script/PDB_analyser.py
@@ -0,0 +1,86 @@
+#! /home/cofia/miniconda3/bin/python
+
+"""
+This script analyses a pdb file looking at its title, information, making a histogram, secondary structure
+and exporting the file. Note that you have to load the file first to input the other options of analysis
+Python Version: 3.7.2 
+
+"""
+
+file_name = '  None  ' 
+
+import modules #modules created that have all the functions in the options 
+
+def menu(file_name):
+    """
+    This function returns menu
+    """
+    print (80 * '*')
+    print ('*' + 1*' ' + 'PDB FILE ANALYZER' + 60*' ' + '*')
+    print (80 * '*')
+    print ('*'+ 1*' ' + 'Select an option from below:' + 49*' ' + '*')
+    print ('*' + 78*' ' + '*')
+    print ('*' + 6*' ' + '1) Open a PDB File' + 23*' ' + '(O)' + 28*' ' + '*')
+    print ('*' + 6*' ' + '2) Information'+ 27*' ' + '(I)' + 28*' ' + '*')
+    print ('*' + 6*' ' + '3) Show histogram of amino acids '+ 8*' ' + '(H)' + 28*' ' + '*')
+    print ('*' + 6*' ' + '4) Display Secondary Structure'+ 11*' ' + '(S)' + 28*' ' + '*')
+    print ('*' + 6*' ' + '5) Export PDB File '+ 22*' ' + '(X)' + 28*' ' + '*')
+    print ('*' + 6*' ' + '6) Exit '+ 33*' ' + '(Q)' + 28*' ' + '*')
+    print ('*' + 55*' ' + 'Current PDB: %s  ' %file_name + '*')
+    print (80 * '*')
+    
+menu(file_name)
+
+def input_options():
+    """
+    This functions returns options that you want to choose for protein file analysis
+    """
+    option = input (""" 
+    From the Menu, below are the options for you to input. 
+    Kindly provide input from the options provided 
+                        O: Open a PDB file
+                        I: Information 
+                        H: Show histogram of amino acids
+                        S: Display Secondary structure 
+                        X: Export PDB File
+                        Q: Exit
+                        
+                        Please Enter your choice: """)
+    if option.lower() == 'o':
+        file_name = modules.option_O()
+        menu(file_name) #replace none with input file loaded 
+        input_options() #call input option back after you are done with the option selected    
+    elif option.lower() == 'i':
+        modules.option_I() #calling modules with this function option_I()
+        input_options() 
+    elif option.lower() == 'h':
+        modules.option_H()
+        input_options()
+    elif option.lower() == 's':
+        modules.option_S()
+        input_options()
+    elif option.lower() == 'x':
+        modules.option_X()
+        input_options()
+    elif option.lower() == 'q':
+        exit2()
+    else:
+        print("Enter correct option as per MENU above....Try again!!!")
+        input_options()
+
+def exit2():
+    """
+    This function is for an exit option 
+    """
+    exit_plan = input('Do you want to exit (E) or Do you want to go back to menu (M)')
+    exit_plan = exit_plan.upper() #change to capital letters 
+    if exit_plan == 'E':
+        print ('Awesome!!! Thank you for using this script to browse your PDB file.')
+    elif exit_plan == 'M':
+        print ('Okay....Taking you back to the menu.')
+        input_options()
+    else:
+        print("Enter correct option either E or M ....Try again!!!")
+        exit2()     
+
+input_options()
